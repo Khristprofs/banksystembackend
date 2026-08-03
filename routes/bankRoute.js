963556a606b825c458ed79306843a86425a8995d
Nweke Chinonso Christian <nwekechristian890@gmail.com>
2026-08-03 15:32:37 +0100

    made changes

--- routes/bankRoute.js
+++ routes/bankRoute.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bankController = require('../controller/bankController');
 const router = express.Router();
 
-router.post("/create", bankController.creatBank);
+router.post("/create", bankController.createBank);
 router.get("/", bankController.getBanks);
 router.get("/:id", bankController.getBankById);
-router.get("/:id/delete", bankController.getBankByName)
 router.get("/:id/update", bankController.updateBank);
+router.get("/:id/delete", bankController.deleteBank);
 
 
 module.exports = router;
